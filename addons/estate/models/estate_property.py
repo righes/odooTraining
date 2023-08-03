4521dd7c0a73661f76d3ee0dd302e4e8c5361540
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-03 05:52:05 +0300

    update property model and views

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -41,3 +41,5 @@ class EstateProperty(models.Model):
         copy=False,
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    seller_id = fields.Many2one('res.users', string="Salesman", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one('res.partner', string="Buyer", copy=False)
