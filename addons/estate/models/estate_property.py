f0ee8785db3476c9ee82076dbad681d76f568817
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-03 07:17:34 +0300

    add tags to property model and views

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -41,5 +41,6 @@ class EstateProperty(models.Model):
         copy=False,
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     seller_id = fields.Many2one('res.users', string="Salesman", default=lambda self: self.env.user)
     buyer_id = fields.Many2one('res.partner', string="Buyer", copy=False)
