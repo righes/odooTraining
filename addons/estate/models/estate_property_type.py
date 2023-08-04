a59a80f138bc1531ec27e2fd9c9c8b7f7d043541
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-04 06:21:38 +0300

    add properties list to property type form

--- addons/estate/models/estate_property_type.py
+++ addons/estate/models/estate_property_type.py
@@ -5,6 +5,7 @@ class EstatePropertyType(models.Model):
     _description = "Estate Property Type Model"
 
     name = fields.Char("Name", required = True)
+    property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
 
     _sql_constraints = [
         ("check_name", "UNIQUE(name)", "The name must be unique!"),
