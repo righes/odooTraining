2523affef5475b543d6de447d69cf44616770f50
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-11 06:40:17 +0300

    add systray icon for t-shirt orders

--- addons/awesome_tshirt/static/src/tshirt_service.js
+++ addons/awesome_tshirt/static/src/tshirt_service.js
@@ -2,13 +2,14 @@
 
 import { registry } from "@web/core/registry";
 import { memoize } from "@web/core/utils/functions";
+import { session } from "@web/session";
 
 export const tshirtService = {
   dependencies: ["rpc"],
   async: ["loadStatistics"],
   start(env, { rpc }) {
     return {
-      loadStatistics: memoize(() => rpc("/awesome_tshirt/statistics")),
+      loadStatistics: memoize(() => session.tshirt_statistics || rpc("/awesome_tshirt/statistics")),
     };
   },
 };
