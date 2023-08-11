bf84b6546f68ed0dfacdf891fc258230d197ae83
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-11 06:56:22 +0300

    add a command to the command palette

--- addons/awesome_tshirt/static/src/image_preview_field/image_preview_field.js
+++ addons/awesome_tshirt/static/src/image_preview_field/image_preview_field.js
@@ -1,10 +1,17 @@
 /** @odoo-module */
 
 import { Component } from "@odoo/owl";
+import { useCommand } from "@web/core/commands/command_hook";
 import { registry } from "@web/core/registry";
 import { CharField } from "@web/views/fields/char/char_field";
 
-class ImagePreviewField extends Component {}
+class ImagePreviewField extends Component {
+  setup() {
+    useCommand("Open image in a new tab", () => {
+      window.open(this.props.value, "_blank");
+    });
+  }
+}
 
 ImagePreviewField.template = "awesome_tshirt.ImagePreviewField";
 ImagePreviewField.components = { CharField };
