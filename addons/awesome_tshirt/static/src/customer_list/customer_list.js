3a06e354062920ae193b07f63d4b755d26a5984c
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-14 13:05:54 +0300

    add pagination to customer list

--- addons/awesome_tshirt/static/src/customer_list/customer_list.js
+++ addons/awesome_tshirt/static/src/customer_list/customer_list.js
@@ -4,18 +4,22 @@ import { Component, onWillStart, useState } from "@odoo/owl";
 import { KeepLast } from "@web/core/utils/concurrency";
 import { useService } from "@web/core/utils/hooks";
 import { fuzzyLookup } from "@web/core/utils/search";
+import { Pager } from "@web/core/pager/pager";
 
 export class CustomerList extends Component {
   setup() {
     this.orm = useService("orm");
     this.partners = useState({ data: [] });
+    this.pager = useState({ offset: 0, limit: 20 });
     this.keepLast = new KeepLast();
     this.state = useState({
       searchString: "",
       displayActiveCustomers: false,
     });
     onWillStart(async () => {
-      this.partners.data = await this.loadCustomers();
+      const { length, records } = await this.loadCustomers();
+      this.partners.data = records;
+      this.pager.total = length;
     });
   }
 
@@ -25,12 +29,19 @@ export class CustomerList extends Component {
 
   async onChangeActiveCustomers(ev) {
     this.state.displayActiveCustomers = ev.target.checked;
-    this.partners.data = await this.keepLast.add(this.loadCustomers());
+    this.pager.offset = 0;
+    const { length, records } = await this.keepLast.add(this.loadCustomers());
+    this.partners.data = records;
+    this.pager.total = length;
   }
 
   loadCustomers() {
+    const { limit, offset } = this.pager;
     const domain = this.state.displayActiveCustomers ? [["has_active_order", "=", true]] : [];
-    return this.orm.searchRead("res.partner", domain, ["display_name"]);
+    return this.orm.webSearchRead("res.partner", domain, ["display_name"], {
+      limit: limit,
+      offset: offset,
+    });
   }
 
   filterCustomers(name) {
@@ -40,8 +51,16 @@ export class CustomerList extends Component {
       return this.partners.data;
     }
   }
+
+  async onUpdatePager(newState) {
+    Object.assign(this.pager, newState);
+    const { records } = await this.loadCustomers();
+    this.partners.data = records;
+    this.filterCustomers(this.filterName);
+  }
 }
 
+CustomerList.components = { Pager };
 CustomerList.template = "awesome_tshirt.CustomerList";
 
 CustomerList.props = {
