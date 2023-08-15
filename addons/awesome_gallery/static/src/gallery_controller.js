7b19d5f61bc910a29202aabf817ea0e48dd99139
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-15 05:30:36 +0300

    add gallery model and renderer to gallery view

--- addons/awesome_gallery/static/src/gallery_controller.js
+++ addons/awesome_gallery/static/src/gallery_controller.js
@@ -1,33 +1,25 @@
 /** @odoo-module */
 
 import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
-import { KeepLast } from "@web/core/utils/concurrency";
 import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
 
 export class GalleryController extends Component {
   setup() {
     this.orm = useService("orm");
-    this.images = useState({ data: [] });
-    this.keeplast = new KeepLast();
+
+    this.model = useState(new this.props.Model(this.orm, this.props.resModel, this.props.archInfo, this.props.domain));
+
     onWillStart(async () => {
-      const { records } = await this.loadImages(this.props.domain);
-      this.images.data = records;
+      await this.model.load();
     });
+
     onWillUpdateProps(async (nextProps) => {
       if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
-        const { records } = await this.loadImages(nextProps.domain);
-        this.images.data = records;
+        this.model.domain = nextProps.domain;
+        await this.model.load();
       }
     });
-  }
-
-  loadImages(domain) {
-    return this.keeplast.add(
-      this.orm.webSearchRead(this.props.resModel, domain, [this.props.archInfo.imageField], {
-        limit: this.props.archInfo.limit,
-      })
-    );
   }
 }
 
---
+++ addons/awesome_gallery/static/src/gallery_renderer.js
@@ -0,0 +1,7 @@
+/** @odoo-module */
+
+import { Component } from "@odoo/owl";
+
+export class GalleryRenderer extends Component {}
+
+GalleryRenderer.template = "awesome_gallery.Renderer";
