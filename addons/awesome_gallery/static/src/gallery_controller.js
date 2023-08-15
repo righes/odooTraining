e96e7fa9a39d42e627d82d1676ade56ec955e9d6
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-15 05:24:07 +0300

    update gallery controller

--- addons/awesome_gallery/static/src/gallery_controller.js
+++ addons/awesome_gallery/static/src/gallery_controller.js
@@ -1,9 +1,35 @@
 /** @odoo-module */
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
+import { KeepLast } from "@web/core/utils/concurrency";
+import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
 
-export class GalleryController extends Component {}
+export class GalleryController extends Component {
+  setup() {
+    this.orm = useService("orm");
+    this.images = useState({ data: [] });
+    this.keeplast = new KeepLast();
+    onWillStart(async () => {
+      const { records } = await this.loadImages(this.props.domain);
+      this.images.data = records;
+    });
+    onWillUpdateProps(async (nextProps) => {
+      if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
+        const { records } = await this.loadImages(nextProps.domain);
+        this.images.data = records;
+      }
+    });
+  }
+
+  loadImages(domain) {
+    return this.keeplast.add(
+      this.orm.webSearchRead(this.props.resModel, domain, [this.props.archInfo.imageField], {
+        limit: this.props.archInfo.limit,
+      })
+    );
+  }
+}
 
 GalleryController.template = "awesome_gallery.View";
 GalleryController.components = { Layout };
