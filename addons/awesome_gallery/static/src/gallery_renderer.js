c07a9ca8ba4aee57ba0f80ae96d6201fcc338ed1
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-15 05:39:13 +0300

    add on click event to gallery images

--- addons/awesome_gallery/static/src/gallery_renderer.js
+++ addons/awesome_gallery/static/src/gallery_renderer.js
@@ -1,9 +1,18 @@
 /** @odoo-module */
 
 import { Component } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 import { GalleryImage } from "./gallery_image/gallery_image";
 
-export class GalleryRenderer extends Component {}
+export class GalleryRenderer extends Component {
+  setup() {
+    this.action = useService("action");
+  }
+
+  onImageClick(resId) {
+    this.action.switchView("form", { resId });
+  }
+}
 
 GalleryRenderer.components = { GalleryImage };
 GalleryRenderer.template = "awesome_gallery.Renderer";
