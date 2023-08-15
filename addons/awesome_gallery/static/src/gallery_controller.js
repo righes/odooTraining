05392dd2807d10edd4d53da5118f446faad4bd4a
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-15 05:51:08 +0300

    add pagination to gallery

--- addons/awesome_gallery/static/src/gallery_controller.js
+++ addons/awesome_gallery/static/src/gallery_controller.js
@@ -3,12 +3,26 @@
 import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { Layout } from "@web/search/layout";
+import { usePager } from "@web/search/pager_hook";
 
 export class GalleryController extends Component {
   setup() {
     this.orm = useService("orm");
 
     this.model = useState(new this.props.Model(this.orm, this.props.resModel, this.props.fields, this.props.archInfo, this.props.domain));
+
+    usePager(() => {
+      return {
+        offset: this.model.pager.offset,
+        limit: this.model.pager.limit,
+        total: this.model.recordsLength,
+        onUpdate: async ({ offset, limit }) => {
+          this.model.pager.offset = offset;
+          this.model.pager.limit = limit;
+          await this.model.load();
+        },
+      };
+    });
 
     onWillStart(async () => {
       await this.model.load();
