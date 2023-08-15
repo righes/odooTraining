7e72dde11a8544ec3ff2df3cb422185db6c23a72
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-15 05:13:29 +0300

    add layout to gallery view

--- addons/awesome_gallery/static/src/gallery_controller.js
+++ addons/awesome_gallery/static/src/gallery_controller.js
@@ -1,7 +1,9 @@
 /** @odoo-module */
 
 import { Component } from "@odoo/owl";
+import { Layout } from "@web/search/layout";
 
 export class GalleryController extends Component {}
 
 GalleryController.template = "awesome_gallery.View";
+GalleryController.components = { Layout };
