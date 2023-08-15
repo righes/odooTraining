ebaf68a90aaeb55c89b12de41bf8f848de696c73
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-08-15 05:34:28 +0300

    add images to gallery

--- addons/awesome_gallery/static/src/gallery_renderer.js
+++ addons/awesome_gallery/static/src/gallery_renderer.js
@@ -1,7 +1,9 @@
 /** @odoo-module */
 
 import { Component } from "@odoo/owl";
+import { GalleryImage } from "./gallery_image/gallery_image";
 
 export class GalleryRenderer extends Component {}
 
+GalleryRenderer.components = { GalleryImage };
 GalleryRenderer.template = "awesome_gallery.Renderer";
